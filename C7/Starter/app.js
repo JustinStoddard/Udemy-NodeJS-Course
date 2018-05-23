8a2f2f4ce078842bae9c4a0cb33c14b2638681f7
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-05-23 14:13:37 -0600

    Added New File

---
+++ C7/Starter/app.js
@@ -0,0 +1,25 @@
+// Your Javascript Code Goes Here
+
+
+//Function Statment
+function greet() {
+  console.log('hi');
+}
+greet();
+
+//functions are first-class
+function logGreeting(fn) {
+  fn();
+}
+logGreeting(greet);
+
+//function expression
+var greetMe = function() {
+  console.log('Hi Justin');
+}
+greetMe();
+
+//Its first class
+logGreeting(greetMe);
+
+// Note: The 'starter' folders that appear in this course's downloadable source code is here to give you a starting point to try out coding yourself. The 'finished' folder contains the code as it is when we finish the lecture. You can use the 'finished' folder to compare to your own code, or just examine the finished code.
