224e5a0019d6302d8c6c1534b0569fcff9925a6b
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-05-23 15:14:10 -0600

    Scoping Variables

---
+++ D2/app.js
@@ -0,0 +1,11 @@
+var firstname = 'Jane';
+
+(function () { //This creats a function and immediatly runs it.
+  var firstname = 'John';
+  console.log(firstname);a
+  console.log(lastname);
+
+}('Doe'));
+
+console.log(firstname);
+
