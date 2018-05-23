de283c94e52530ca47f5006adb1bdcd1d485f59e
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-05-23 14:20:32 -0600

    Added New Lecture File and a gitignore

--- C7/Starter/app.js
+++ C7/Starter/app.js
@@ -22,4 +22,9 @@ greetMe();
 //Its first class
 logGreeting(greetMe);
 
+//Use a function expression on the fly
+logGreeting(function() {
+  console.log('Hello Justin');
+})
+
 // Note: The 'starter' folders that appear in this course's downloadable source code is here to give you a starting point to try out coding yourself. The 'finished' folder contains the code as it is when we finish the lecture. You can use the 'finished' folder to compare to your own code, or just examine the finished code.
